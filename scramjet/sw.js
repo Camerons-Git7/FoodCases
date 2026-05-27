9993b1b922d7e28590517c542458abdafeea8ab5
Someones_Git7 <29richmoca@gowcs.net>
2026-05-27 15:49:04 +0000

    Add files via upload

---
+++ scramjet/sw.js
@@ -0,0 +1,24 @@
+importScripts("/scramjet.all.js");
+
+const { ScramjetServiceWorker } = $scramjetLoadWorker();
+const scramjet = new ScramjetServiceWorker();
+
+self.addEventListener("install", (event) => {
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+    event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        (async () => {
+            await scramjet.loadConfig();
+            if (scramjet.route(event)) {
+                return scramjet.fetch(event);
+            }
+            return fetch(event.request);
+        })()
+    );
+});
