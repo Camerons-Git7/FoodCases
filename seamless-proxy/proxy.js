8df61cc2880c2a2df05b4cb5b52013f7777eaee4
Someones_Git7 <29richmoca@gowcs.net>
2026-06-26 20:13:14 -0400

    Update proxy.js

--- seamless-proxy/proxy.js
+++ seamless-proxy/proxy.js
@@ -10,7 +10,7 @@ async function go() {
 
 // Register Service Worker with subfolder support
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('sw.js', { scope: './' })
+    navigator.serviceWorker.register('sw.mjs', { scope: './' })
         .then(() => console.log('Service Worker registered for subfolder'))
         .catch(err => console.error('SW registration failed', err));
 }
